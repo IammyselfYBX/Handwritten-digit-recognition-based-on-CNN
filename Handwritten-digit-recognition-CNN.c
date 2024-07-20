4ae66a3a1332e691cf17f77a025d6cdec699e82e
tony <2252176220@qq.com>
2024-07-20 10:01:48 +0800

    修改文件结构

--- Handwritten-digit-recognition-CNN.c
+++ Handwritten-digit-recognition-CNN.c
@@ -1,8 +1,8 @@
 /*****************************************************************************
 File name: Handwritten-digit-recognition
 Description: CNN手写数字识别
-Author: 甄继伟
-Version: 1.1
+Author: XXX
+Version: 3.1
 Date: 1999年13月32日 
 *****************************************************************************/
 #include <stdio.h>
@@ -15,13 +15,16 @@ Date: 1999年13月32日
 #define SAMPLE_NUM 30
 double lr;
 double result[11];
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
 
 struct parameter{
     double conv_kernel11[3][3]; 
-    double conv_kernel12[3][3]; 
     double conv_kernel21[3][3]; 
-    double conv_kernel22[3][3]; 
     double conv_kernel31[3][3]; 
+    double conv_kernel12[3][3]; 
+    double conv_kernel22[3][3]; 
     double conv_kernel32[3][3]; 
     double fc_hidden_layer1[1152][180]; 
     double fc_hidden_layer2[180][45];   
@@ -39,14 +42,10 @@ struct result{
     double flatten_conv[1][1152]; 
     double first_fc[1][180];      
     double first_relu[1][180];    
-    double second_fc[1][45];     
-    double second_relu[1][45];   
-    double outmlp[1][10];        
-    double result[10];           
-};
-
-struct input{
-    double a[10][SAMPLE_NUM][30][30];
+    double second_fc[1][45];      
+    double second_relu[1][45];    
+    double outmlp[1][10];         
+    double result[10];            
 };
 
 struct sample{
@@ -55,9 +54,9 @@ struct sample{
 }Sample[SAMPLE_NUM*10];
 
 void Conv2d(int w,int h,int k,double *input_matrix,double *kernel,double *out_matrix){
-    for(int i=0;i<w-k+1;i++)
-        for(int j=0;j<h-k+1;j++){
-            out_matrix[i*(w-k+1)+j]=0;
+    for(int i=0;i<w-k+1;i++)        
+        for(int j=0;j<h-k+1;j++){   
+            out_matrix[i*(w-k+1)+j]=0; 
             for(int row=i;row<i+3;row++)
                 for(int col=j;col<j+3;col++)
                     out_matrix[i*(w-k+1)+j]+=input_matrix[row*w+col]*kernel[(row-i)*k+(col-j)];
@@ -93,9 +92,9 @@ void MatrixExtensionImproved(int w,int h,double *input_matrix1,double *input_mat
 }
 
 void MatrixMultiply(int w,int h,int out_deminsion,double *input_matrix,double *para_layer,double*output_matrix){
-    for(int i=0;i<w;i++)
-        for(int j=0;j<out_deminsion;j++){
-            output_matrix[i*w+j]=0;
+    for(int i=0;i<w;i++) 
+        for(int j=0;j<out_deminsion;j++){ 
+            output_matrix[i*w+j]=0; 
             for(int k=0;k<h;k++)
                 output_matrix[i*w+j]+=input_matrix[i*w+k]*para_layer[k*out_deminsion+j];
         }
@@ -124,7 +123,7 @@ void MatrixBackPropagationMultiply(int w,int h,double *para,double *grad,double 
 
 void CalculateMatrixGrad(int w,int h,double *input_matrix,double *grad,double *output_matrix){
     for(int i=0;i<w;i++){
-        output_matrix[i]=0;
+        output_matrix[i]=0;//梯度清空，方便累加
         for(int j=0;j<h;j++){
             output_matrix[i]+=input_matrix[i*h+j]*grad[j];
         }
@@ -140,7 +139,7 @@ void ReluBackPropagation(int w,double *input_matrix,double *grad,double *output_
 void Padding(int w,int stride,double *input_matrix,double *output_matrix){
     for(int i=0;i<w+2*stride;i++)
         for(int j=0;j<w+2*stride;j++)
-            output_matrix[i*(w+2*stride)+j]=0;
+            output_matrix[i*(w+2*stride)+j]=0;//输出矩阵初始化
 }
 
 void OverturnKernel(int k,double *input_matrix,double *output_matrix){
@@ -153,7 +152,6 @@ void MemoryFree(double *x){
     free(x);
     x=NULL;
 }
-
 void init(struct parameter *para){
     srand(time(NULL));
     for(int i=0;i<3;i++)
@@ -184,23 +182,19 @@ void init(struct parameter *para){
         for(int j=0;j<10;j++)
             para->fc_hidden_layer3[i][j]=(rand()/(RAND_MAX+1.0))/10;
 }
-
 void forward(double *input_matrix,struct parameter* para,struct result* data){
     Conv2d(30,30,3,input_matrix,&para->conv_kernel11[0][0],&data->first_conv1[0][0]);
     Conv2d(28,28,3,&data->first_conv1[0][0],&para->conv_kernel21[0][0],&data->sencond_conv1[0][0]);
-    Conv2d(26,26,3,&data->sencond_conv1[0][0],&para->conv_kernel31[0][0],&data->third_conv1[0][0]);
-
+    Conv2d(26,26,3,&data->sencond_conv1[0][0],&para->conv_kernel31[0][0],&data->third_conv1[0][0]);//第一个通道得到24*24的特征图
     Conv2d(30,30,3,input_matrix,&para->conv_kernel12[0][0],&data->first_conv2[0][0]);
     Conv2d(28,28,3,&data->first_conv2[0][0],&para->conv_kernel22[0][0],&data->sencond_conv2[0][0]);
-    Conv2d(26,26,3,&data->sencond_conv2[0][0],&para->conv_kernel32[0][0],&data->third_conv2[0][0]);
-
-    MatrixExtensionImproved(24,24,&data->third_conv1[0][0],&data->third_conv2[0][0],&data->flatten_conv[0][0]);
+    Conv2d(26,26,3,&data->sencond_conv2[0][0],&para->conv_kernel32[0][0],&data->third_conv2[0][0]);//第二个通道得到24*24的特征图
+    MatrixExtensionImproved(24,24,&data->third_conv1[0][0],&data->third_conv2[0][0],&data->flatten_conv[0][0]); // 扁平化
     MatrixMultiply(1,1152,180,&data->flatten_conv[0][0],&para->fc_hidden_layer1[0][0],&data->first_fc[0][0]);
-    Relu(1,180,&data->first_fc[0][0],&data->first_relu[0][0]);
+    Relu(1,180,&data->first_fc[0][0],&data->first_relu[0][0]); 
     MatrixMultiply(1,180,45,&data->first_relu[0][0],&para->fc_hidden_layer2[0][0],&data->second_fc[0][0]);
     Relu(1,45,&data->second_fc[0][0],&data->second_relu[0][0]);
     MatrixMultiply(1,45,10,&data->second_relu[0][0],&para->fc_hidden_layer3[0][0],&data->outmlp[0][0]);
-
     double probability;
     for(int i=0;i<10;i++)
         probability+=exp(data->outmlp[0][i]);
@@ -208,14 +202,12 @@ void forward(double *input_matrix,struct parameter* para,struct result* data){
         data->result[i]=exp(data->outmlp[0][i])/probability;
         result[i]=data->result[i];
     }
-    return;
 }
 
 void backward(int label,struct parameter* para,struct result* data){
     int double_len=sizeof(double);
     double *out_grad;
     out_grad=(double*)malloc(10*double_len);
-
     for(int i=0;i<10;i++)
         if(i==label) out_grad[i]=data->result[i]-1;
         else out_grad[i]=data->result[i]-0;
@@ -248,7 +240,6 @@ void backward(int label,struct parameter* para,struct result* data){
     all_conv_grad=(double*)malloc(1152*double_len);
     CalculateMatrixGrad(1152,180,&para->fc_hidden_layer1[0][0],first_grad,all_conv_grad);
     MemoryFree(first_grad);
-
     double *third_conv_grad1;
     third_conv_grad1=(double*)malloc(576*double_len);
     double *third_conv_grad2;
@@ -289,7 +280,6 @@ void backward(int label,struct parameter* para,struct result* data){
     first_kernel_grad=(double*)malloc(9*double_len);
     Conv2d(30,30,28,&data->mnist_data[0][0],first_conv_grad,first_kernel_grad);
     MemoryFree(first_conv_grad);
-    
     double *third_kernel_grad2;
     third_kernel_grad2=(double*)malloc(9*double_len);
     Conv2d(26,26,24,&data->sencond_conv2[0][0],third_conv_grad2,third_kernel_grad2);
@@ -322,19 +312,15 @@ void backward(int label,struct parameter* para,struct result* data){
     double *first_kernel_grad2;
     first_kernel_grad2=(double*)malloc(9*double_len);
     Conv2d(30,30,28,&data->mnist_data[0][0],first_conv_grad2,first_kernel_grad2);
-
     MatrixBackPropagation(3,3,first_kernel_grad,&para->conv_kernel11[0][0]);
     MatrixBackPropagation(3,3,second_kernel_grad,&para->conv_kernel21[0][0]);
     MatrixBackPropagation(3,3,third_kernel_grad,&para->conv_kernel31[0][0]);
-
     MatrixBackPropagation(3,3,first_kernel_grad2,&para->conv_kernel12[0][0]);
     MatrixBackPropagation(3,3,second_kernel_grad2,&para->conv_kernel22[0][0]);
     MatrixBackPropagation(3,3,third_kernel_grad2,&para->conv_kernel32[0][0]);
-
     MatrixBackPropagation(1152,180,first_wgrad,&para->fc_hidden_layer1[0][0]);
     MatrixBackPropagation(180,45,second_wgrad,&para->fc_hidden_layer2[0][0]);
     MatrixBackPropagation(45,10,out_wgrad,&para->fc_hidden_layer3[0][0]);
-
     MemoryFree(first_kernel_grad);
     MemoryFree(second_kernel_grad);
     MemoryFree(third_kernel_grad);
@@ -347,16 +333,17 @@ void backward(int label,struct parameter* para,struct result* data){
     return;
 }
 
-int DataLoader() {
-    for (int num = 0; num < 10; num++) {
-        for (int i = 0; i < SAMPLE_NUM; i++) {
-            char *e = (char *)malloc(sizeof(char) * 120);
-            int *l = (int *)malloc(sizeof(int) * 960);
+BOOL DataLoader() {
+    for (int num = 0; num < 10; num++) { 
+        for (int i = 0; i < SAMPLE_NUM; i++) { 
+            char *e = (char *)malloc(sizeof(char) * 120);    
+
+            int *l = (int *)malloc(sizeof(int) * 960);      
             if (e == NULL || l == NULL) {
-                printf("内存分配失败！\n");
-                free(e); 
+                perror("内存分配失败！\n");
+                free(e);        
                 free(l);
-                return 1;
+                return FALSE;
             }
 
             char route_name[50] = "Training_set/";
@@ -365,12 +352,13 @@ int DataLoader() {
             strcat(route_name, file_name);
 
             FILE *fp = fopen(route_name, "rb");
-            if (fp == NULL) {
-                printf("未能打开训练集数据. 检查'Training_set'文件夹是否存在，并且文件夹中有图片!\n");
+            if (!fp) {
+                perror("未能打开训练集数据. 检查'Training_set'文件夹是否存在，并且文件夹中有图片!\n");
                 free(e);
                 free(l);
-                return 1;
+                return FALSE;
             }
+
             fseek(fp, 62, SEEK_SET);
             fread(e, sizeof(char), 120, fp);
             fclose(fp);
@@ -384,7 +372,7 @@ int DataLoader() {
                         break;
                 }
             }
-
+           
             int g = 0;
             for (int u = 0; u < 30; u++) {
                 y = 0;
@@ -405,44 +393,48 @@ int DataLoader() {
                     }
                 }
             }
-
+            
             Sample[num * SAMPLE_NUM + i].number = num;
             free(e);
             free(l);
         }
     }
-    return 0;
+    return TRUE;
 }
 
-int read_file(struct parameter* parameter4){
+BOOL read_file(struct parameter* parameter_dest){
     FILE*fp;
-    fp=fopen("Training_set/Network_parameter.bin","rb");
+    fp=fopen("network_parameter.txt","rb");
     if(fp==NULL)
     {
         printf("文件打开失败，请检查网络参数文件是否在训练集文件夹内！\n");
-        return 1;
+        return FALSE;
     }
-    struct parameter* parameter1;
-    parameter1=(struct parameter*)malloc(sizeof(struct parameter));
-    fread(parameter1,sizeof(struct parameter),1,fp);
-    (*parameter4)=(*parameter1);
+    struct parameter* parameter_tmp=NULL;
+    parameter_tmp=(struct parameter*)malloc(sizeof(struct parameter));
+    fread(parameter_tmp,sizeof(struct parameter),1,fp);
+    (*parameter_dest)=(*parameter_tmp);
     fclose(fp);
-    free(parameter1);
-    parameter1=NULL;
-    return 0;
+    free(parameter_tmp);
+    parameter_tmp=NULL;
+
+    return TRUE;
 }
 
-void printf_file(struct parameter* parameter4){
+BOOL write_para_to_file(struct parameter* parameter_file){
     FILE*fp;
-    fp=fopen("Training_set//Network_parameter.bin","wb");
-    struct parameter* parameter1;
-    parameter1=(struct parameter*)malloc(sizeof(struct parameter));
-    (*parameter1)=(*parameter4);
-    fwrite(parameter1,sizeof(struct parameter),1,fp);//打印网络结构体
+    fp=fopen("network_parameter.txt","wb");
+    struct parameter* parameter_tmp;
+    parameter_tmp=(struct parameter*)malloc(sizeof(struct parameter));
+
+    (*parameter_tmp)=(*parameter_file);
+    fwrite(parameter_tmp,sizeof(struct parameter),1,fp);
+
     fclose(fp);
-    free(parameter1);
-    parameter1=NULL;
-    return;
+    free(parameter_tmp);
+    parameter_tmp=NULL;
+    
+    return TRUE;
 }
 
 void printf_file2(struct parameter* parameter4){
@@ -464,47 +456,65 @@ double Cross_entropy(double *a,int m){
     return u;
 }
 
+void show_progress_bar(int progress, int total) {
+    int bar_width = 50;
+    float percent_complete = (float)progress / total;
+    int position = bar_width * percent_complete;
+
+    printf("[");
+    for (int i = 0; i < bar_width; ++i) {
+        if (i < position) {
+            printf("=");
+        } else if (i == position) {
+            printf(">");
+        } else {
+            printf(" ");
+        }
+    }
+    printf("] %d%%\r", (int)(percent_complete * 100));
+    fflush(stdout);
+}
+
 void train(int epochs,struct parameter *para,struct result *data){
-    printf("\t进入train函数\n");
-    double corss_loss=2;
+    double corss_loss=2; 
     for(int epoch=0;epoch<epochs;epoch++){
         lr=pow((corss_loss/10),1.7);
-        if(lr>0.01) lr=0.01;
+        if(lr>0.01) {
+            lr=0.01;
+        }
+
+        show_progress_bar(epoch+1, epochs);
         if((epoch+1)%10==0){
-            printf("训练进度: %lf",100*((double)(epoch+1)/(double)epochs));
-            printf("%%  交叉熵损失: %lf  学习率:%.10lf\n",corss_loss,lr);
+            fflush(stdout);
+            printf("\t交叉熵损失: %lf  学习率:%.10lf\n",corss_loss,lr);
             if(lr<0.0000000001)printf_file2(para);
         }
 
         int a,b;
         srand(time(NULL));
-        for(int q=0;q<300;q++){
+        struct sample* sample_tmp = NULL; 
+        sample_tmp=(struct sample *)malloc(sizeof(struct sample));
+        for(int q=0;q<SAMPLE_NUM*10;q++){
             a=(int)((rand()/(RAND_MAX+1.0))*300);
             b=(int)((rand()/(RAND_MAX+1.0))*300);
             if(a>=0&&a<300&&(a!=b)&&b>=0&&b<300){
-                struct sample* sample5;
-                sample5=(struct sample *)malloc(sizeof(struct sample));
-                (*sample5)=Sample[a];
+                (*sample_tmp)=Sample[a];
                 Sample[a]=Sample[b];
-                Sample[b]=(*sample5);
-                free(sample5);
-                sample5=NULL;
+                Sample[b]=(*sample_tmp);
             }
             else continue;
         }
         for(int i=0;i<SAMPLE_NUM*10;i++) {
             corss_loss=0;
-            struct sample* sample3;
-            sample3=(struct sample *)malloc(sizeof(struct sample));
-            (*sample3)=Sample[i];
-            int y=sample3->number;
-            forward(&sample3->a[0][0],para,data);//正向传播
+            (*sample_tmp)=Sample[i];
+            int y=sample_tmp->number;
+            forward(&sample_tmp->a[0][0],para,data);
             backward(y,para,data);
-            free(sample3);
-            sample3=NULL;
             double g=Cross_entropy(&data->result[0],y);
             if(g>corss_loss)corss_loss=g;
         }
+        free(sample_tmp);
+        sample_tmp=NULL;
     }
     printf("\n");
     return;
@@ -517,12 +527,11 @@ void test_network(struct parameter* parameter2,struct result *data2){
     for(int i=0;i<10;i++){
         FILE *fp;
         char s[30];
-        sprintf(s,"%s%d%s","Training_set/Test_set/",i,".bmp");
-        printf("\n打开的文件名:%s\n",s);
+        sprintf(s,"%s%d%s","Test_set/",i,".bmp");
+        printf("\n打开的文件名:%s",s);
         fp = fopen(s, "rb");
-        if(fp == NULL){
-            printf("不能打开文件!\n");
-            system("pause");
+        if(!fp){
+            perror("不能打开文件!\n");
             return;
         }
         fseek(fp, 62, SEEK_SET);
@@ -579,21 +588,26 @@ void test_network(struct parameter* parameter2,struct result *data2){
         printf("\n");
         for(int i=0;i<10;i++)
         {
-            printf("预测值是%d的概率：%lf\n",i,result[i]);
+            printf("\t预测值是%d的概率：%lf\n",i,result[i]);
         }
-        printf("最终预测值:%d\n",k);
+
+        printf("\033[1;31;43m最终预测值: %d \033[0m\n", k);
 }
 return ;
 }
 
-int main(){
+
+int main(int argc, char const *argv[]){
     int h=DataLoader();
-    if(h==0)printf("训练数据读取成功\n");
-    else if(h==1){
+    if(h==TRUE){
+        printf("训练数据读取成功！\n");
+    }
+    else if(h==FALSE){
         printf("训练集读取失败！程序自动退出\n");
         return 0;
     }
 
+    printf("=============================================\n");
     printf("开始训练网络\n");
     struct parameter *storage;
     (storage) = (struct parameter*)malloc(sizeof(struct parameter));
@@ -602,28 +616,28 @@ int main(){
 
     char g;
     do {
-        printf("请问您是否希望从已训练的网络参数文件中读取网络参数？(是请按y，否请按n): ");
-        setbuf(stdin,NULL);//清空键盘缓冲区
+        printf("请问您是否希望从已训练的网络参数文件中读取网络参数？(是请按 y，否请按 n): ");
+        setbuf(stdin,NULL);
         g=getchar();
         while(getchar() != '\n'); 
         if(g=='y'){
             int h=read_file(storage);
-            if(h==1){
+            if(h==FALSE){
                 printf("参数包不存在！开始自动随机初始化网络参数\n");
                 init(storage);
-                printf_file(storage);
+                write_para_to_file(storage);
                 printf("网络参数初始化完毕！\n");
-                printf("网络参数已保存到 Network_parameter.bin 文件中\n");
-            }
-            if(h==0)
+                printf("网络参数已保存到 network_parameter.txt 文件中\n");
+            }else if(h==TRUE){
                 printf("参数读取成功!\n");
-        }
-        else if (g=='n'){
+            }
+        }else if (g=='n'){
             init(storage);
-            printf_file(storage);
+            write_para_to_file(storage);
             printf("参数初始化完毕！\n");
         }
     } while (g != 'y' && g != 'n');
+    printf("=============================================\n");
 
     int epoch;
     char v;
@@ -632,8 +646,9 @@ int main(){
         scanf("%d",&epoch);
         printf("开始训练\n");
         train(epoch,storage,data);
+        printf("开始测试\n");
         test_network(storage,data);
-        printf_file(storage);
+        write_para_to_file(storage);
 
         do {
             printf("继续训练请按回车，退出请按q: ");
@@ -641,7 +656,7 @@ int main(){
             v=getchar();
             while(getchar() != '\n'); 
             if(v=='q'){
-                printf_file(storage);
+                write_para_to_file(storage);
                 return 0;
             }else if (v=='\n') {
                 break;
@@ -651,6 +666,8 @@ int main(){
         } while (1);
     } while (v != 'q');
 
+    free(storage);
+    free(data);
+
     return 0;
 }
-
